b49e2dd9f686b3086144bb68dbfbeffa4e9e7661
MD MOTAHHARUL HAQUE <mdmotahharul.haque@gmail.com>
2020-10-11 22:37:51 +0530

    Insertion sort algorithms

---
+++ Sorting_Algo/src/main/java/InsertionSort.java
@@ -0,0 +1,24 @@
+public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        int[] inArray = {20, 35, -15, 70, 55, 1, -22};
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < inArray.length; firstUnsortedIndex++) {
+
+            int newElement = inArray[firstUnsortedIndex];
+
+            int i;
+
+            for (i = firstUnsortedIndex; i > 0 && inArray[i - 1] > newElement ; i--) {
+                inArray[i] = inArray[i - 1];
+            }
+
+            inArray[i] = newElement;
+        }
+        
+        for (int element:inArray) {
+            System.out.print(element + " ");
+        }
+    }
+}
