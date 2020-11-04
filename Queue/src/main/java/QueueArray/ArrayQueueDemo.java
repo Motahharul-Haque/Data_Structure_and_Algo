89740cd7e626504090112c4810f5ad9ebf986307
MD MOTAHHARUL HAQUE <mdmotahharul.haque@gmail.com>
2020-11-04 08:31:56 +0530

    problem on queue data structure

--- Queue/src/main/java/QueueArray/ArrayQueueDemo.java
+++ Queue/src/main/java/QueueArray/ArrayQueueDemo.java
@@ -12,21 +12,30 @@ public class ArrayQueueDemo {
         Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
         Employee billEnd = new Employee("Bill", "End", 78);
 
-        ArrayQueue queue = new ArrayQueue(10);
+        /*ArrayQueue queue = new ArrayQueue(10);
         queue.add(janeJones);
         queue.add(johnDoe);
         queue.add(marySmith);
         queue.add(mikeWilson);
-        queue.add(billEnd);
+        queue.add(billEnd);*/
 
         //queue.printQueue();
 
-        System.out.println("Element Removed : "+queue.remove());
-        System.out.println("Element Removed : "+queue.remove());
+        //System.out.println("Element Removed : "+queue.remove());
+        //System.out.println("Element Removed : "+queue.remove());
+
+        ArrayQueue queue = new ArrayQueue(5);
+        queue.add(janeJones);
+        queue.add(johnDoe);
+        queue.remove();
+        queue.add(marySmith);
+        queue.remove();
+        queue.add(mikeWilson);
+        queue.remove();
+        queue.add(billEnd);
+        queue.remove();
+        queue.add(janeJones);
 
         queue.printQueue();
-
-        System.out.println(queue.peek());
-        
     }
 }
