a23c08a71e1be55041f225e935e514e2ebf96920
MD MOTAHHARUL HAQUE <mdmotahharul.haque@gmail.com>
2020-11-18 06:35:19 +0530

    Hashing data structure

---
+++ Hashing_DS/src/main/java/Hash_Table/Using_Array/StoredEmployee.java
@@ -0,0 +1,12 @@
+package Hash_Table.Using_Array;
+
+public class StoredEmployee {
+
+    public String key;
+    public Employee employee;
+
+    public StoredEmployee(String key, Employee employee) {
+        this.key = key;
+        this.employee = employee;
+    }
+}
